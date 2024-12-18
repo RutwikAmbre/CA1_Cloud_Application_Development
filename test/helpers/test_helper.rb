4c458dd46851eee2062a9a25979a6d226bbf84c9
RutwikAmbre <x20225431@student.ncirl.ie>
2024-12-18 14:17:59 +0000

    testing + CI/CD 8th Deployment

--- test/helpers/test_helper.rb
+++
@@ -1,8 +0,0 @@
-def sign_in(user)
-    @user = user
-    visit sign_in_url
-    within '#sign-in' do
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: @user.password
-      click_on 'Sign in'
-    end
